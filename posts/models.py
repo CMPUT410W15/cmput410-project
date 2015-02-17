8026176fa3a50e729783cc5933188d8b604caab7
Neil Borle <nborle@ualberta.ca>
2015-02-16 21:00:20 -0700

    Added content_type and friend to Posts
    This update allows you to store who sent the post and who (if anyone) is to
    receive it. By do this and added a Friend visiblilty you can now send posts
    that are private to one other author. This commit also includes a new
    attribute for content_type, where you can specify markdown or plain text

--- posts/models.py
+++ posts/models.py
@@ -1,20 +1,25 @@
 """Models for the posts application."""
-import uuid
-
 from django.db import models
 from author.models import Author, gen_uuid
 
 
 PRIVATE = 0
-FRIENDS = 1
-FOAF = 2
-PUBLIC = 3
-SERVERONLY = 4
+FRIEND = 1
+FRIENDS = 2
+FOAF = 3
+PUBLIC = 4
+SERVERONLY = 5
 VISIBILITY = ((PRIVATE, 'Private'),
+              (FRIEND, 'Friend'),
               (FRIENDS, 'Friends'),
               (FOAF, 'FriendOfAFriend'),
               (PUBLIC, 'Public'),
               (SERVERONLY, 'ServerOnly'))
+
+PLAINTEXT = 0
+MARKDOWN = 1
+CONTENT_TYPE = ((PLAINTEXT, 'PlainText'),
+                (MARKDOWN, 'Markdown'))
 
 
 # Create your models here.
@@ -27,16 +32,21 @@ class Post(models.Model):
     uid = models.CharField(max_length=36, unique=True, default=gen_uuid)
     title = models.CharField(max_length=36)
     content = models.CharField(max_length=500)
+    content_type = models.IntegerField(choices=CONTENT_TYPE,
+                                       default=PLAINTEXT)
     visibility = models.IntegerField(choices=VISIBILITY)
     published = models.DateTimeField(auto_now_add=True)
 
+    send_author = models.ForeignKey(Author, related_name='sending_authors')
+    receive_author = models.ForeignKey(Author, null=True,
+                                       related_name='receiving_authors')
     image = models.ForeignKey(Image, null=True)
-    author = models.ForeignKey(Author)
 
-    def add_comment(self, commentary):
+    def add_comment(self, author, comment):
         return Comment.objects.create(
-            content=commentary,
-            post=self)
+            author=author,
+            post=self,
+            content=comment)
 
     def remove_comment(self, uid):
         Comment.objects.filter(
