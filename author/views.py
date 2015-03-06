0e5b3c9f0fcbcd58225034b23dc8811edc2f4244
Konrad Lindenbach <konrad.lindenbach@gmail.com>
2015-03-06 02:33:08 -0700

    Resolves #25, 37, #38, #39 Implemented friends page

--- author/views.py
+++ author/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
+from models import *
 
-# Create your views here.
+def friends(request):
+    me = Author.objects.get(user=request.user)
+    authors = Author.objects.all()
+    friends = me.get_friends()
+    following = list(set(me.get_followees()) - set(friends))
+    friend_requests = list(set(me.get_friend_requests()) - set(friends))
+    not_friends = list(
+        set(authors) - set(friends) - set(following) - set(friend_requests) -
+        set([me])
+    )
+
+    return render(
+        request,
+        'friends.html', 
+        {
+            "friends": friends,
+            "following": following,
+            "friend_requests": friend_requests,
+            "not_friends": not_friends,
+        }
+    )
+
+def befriend(request, user):
+    me = Author.objects.get(user=request.user)
+    me.befriend(Author.objects.get(user=User.objects.get(username=user)))
+    return redirect('/friends/')
+
+def unbefriend(request, user):
+    me = Author.objects.get(user=request.user)
+    me.unfollow(Author.objects.get(user=User.objects.get(username=user)))
+    return redirect('/friends/')
+
+def follow(request, user):
+    me = Author.objects.get(user=request.user)
+    me.follow(Author.objects.get(user=User.objects.get(username=user)))
+    return redirect('/friends/')
+
+def unfollow(request, user):
+    me = Author.objects.get(user=request.user)
+    me.unfollow(Author.objects.get(user=User.objects.get(username=user)))
+    return redirect('/friends/')
