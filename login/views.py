b67cc693fe80cc6236423c9208246677f975f346
Neil Borle <nborle@ualberta.ca>
2015-03-08 21:02:50 -0600

    Updated the Stream page to show posts properly
    Was having issues seeing posts to I rewrote the home view so that everything
    displayed properly for me

--- login/views.py
+++ login/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 ### Source material taken from:
 ###		https://mayukhsaha.wordpress.com/2013/05/09/simple-login-and-user-registration-application-using-django/
 ### 	March 2, 2015
-### No explicit license 
+### No explicit license
 from login.forms import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
@@ -15,6 +15,7 @@ from django.template import RequestContext
 
 from author.models import Author
 from posts.models import Post
+from posts.models import PRIVATE, FRIEND, FRIENDS, FOAF, PUBLIC, SERVERONLY
 @csrf_protect
 def register(request):
     #Create both a user and an author every time someone registers
@@ -37,37 +38,45 @@ def register(request):
     variables = RequestContext(request, {
     'form': form
     })
- 
+
     return render_to_response(
     'registration/register.html',
     variables,
     )
- 
+
 def register_success(request):
     return render_to_response(
     'registration/success.html',
     )
- 
+
 def logout_page(request):
     logout(request)
     return HttpResponseRedirect('/')
- 
+
 @login_required
 def home(request):
     #Note: attributes passed in here are all lowercase regardless of capitalization
-    posts= Post.objects.all()
-    author=request.user.author
+    posts = Post.objects.all()
+    author = request.user.author
 
-    #Get everyone's public posts and get the posts from friends they received 
-    public_posts=[]
-    for p in posts:
-        if (p.visibility == 4):
-            public_posts.append(p)
-        else:
-            continue
+    #Get everyone's public posts and get the posts from friends they received
+    public_posts = [p for p in posts if p.visibility == PUBLIC]
+    private_posts = [p for p in author.get_posts(visibility=PRIVATE)]
+    to_me_posts = [p for p in author.get_received_posts()]
 
-    friend_posts=author.get_received_posts(visibility=1)
+    friends_posts = []
+    posts_to_friends = [p for p in posts if p.visibility == FRIENDS]
+    for post in posts_to_friends:
+        if post.send_author in author.get_friends() or post.send_author == author:
+            friends_posts.append(post)
+
+    all_posts = public_posts + private_posts + to_me_posts + friends_posts
+    all_posts = sorted(all_posts, key=lambda x: x.published, reverse=True)
+
     return render(request,
-    'home.html',
-    { 'user': request.user , 'author': request.user.author, 'public_posts':public_posts, 'friend_posts':friend_posts}
-    )
+                  'home.html',
+                  {
+                      'user': request.user,
+                      'author': request.user.author,
+                      'posts': all_posts
+                  })
