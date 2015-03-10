34bf959e4b8b7bbcac822978e8cd8ee56ad15513
root <klindenb@ualberta.ca>
2015-03-09 23:40:20 -0600

    Changed handling of static files

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -107,15 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-# Put strings here, like "/home/html/static" or "C:/www/django/static".
-# Always use forward slashes, even on Windows.
-# Don't forget to use absolute paths, not relative paths.
-
-#Added by Paul for twitter bootstrap
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static',),)
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static',)
 
 '''
 # Added by Ben - May revisit
