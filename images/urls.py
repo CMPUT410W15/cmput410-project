728cfa71d9a0f7bdfc0e68f164e1bef069b19453
Jessica Surya <jsurya@ualberta.ca>
2015-04-02 01:39:53 -0600

    Image upload functionality is complete, endpoints in image app to get all images or query by uid

--- images/urls.py
+++ images/urls.py
@@ -1,7 +1,15 @@
+from django.conf.urls import patterns, include, url
 from django.shortcuts import render
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 import views
 
 # Create your views here.
-urlpatterns = patterns('',
-    url(r'^images/$', views.images, name='images'),
+urlpatterns = patterns('images.views',
+    url(r'^$', views.images, name='images'),
+    url(r'^(?P<uid>\d+)/$', views.get_image),
+
 )
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- images/views.py
+++ images/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from images.models import *
+from django.contrib.sites.models import Site
+from django.http import HttpResponse
 
 # Create your views here.
+def get_image(request, uid):
+    image = Image.objects.get(uid=uid)
+
+    return HttpResponse(image.read())
+
+def images(request):
+    context = RequestContext(request)
+    # Get all images
+    images = Image.objects.all()
+    return render_to_response('all.html', {'images': images}, context)
