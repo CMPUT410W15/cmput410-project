002410071f47cbbe81d7761d266ebe58c4c936f0
Neil Borle <nborle@ualberta.ca>
2015-04-05 23:50:03 -0600

    Added password field to nodes

--- management/models.py
+++ management/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Node(models.Model):
     url = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
+    password = models.CharField(max_length=36)
 
     def __unicode__(self):
         return self.url
